31852f50428a1c3bbb5779c3eadd54e12739ce37
shopmaster1-collab <shopify@master.com.mx>
2025-06-18 16:51:44 -0600

    Update app.py
    cambié a 100 frases

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ base_prompt = PromptTemplate(
         "Mantén la respuesta clara, corta y orientada a ayudar al cliente a tomar una decisión de compra. "
         "En caso de no contar con el artículo solicitado, recomienda otros productos similares. "
         "Si no puedes responder con la información disponible, invita cordialmente a comunicarse con un asesor por Teléfono o WhatsApp al 5580050900, de Lunes a Viernes, de 8:00 am a 5:30 pm. "
-        "Limita tu respuesta a un máximo de 150 palabras, no cortes las frases, procura en tu límite escribir la respuesta comleta. "
+        "Limita tu respuesta a un máximo de 100 palabras, no cortes las frases, procura en tu límite escribir la respuesta comleta. "
     )
 )
 
